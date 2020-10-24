1d1d3e9471da9e2024b35d7e14acb2047d43ca2f
Ayush Srivastava <sayush2807@gmail.com>
2020-10-24 19:22:53 +0530

    Update extract_features.py
    Add Comments

--- Feature Extractor/extract_features.py
+++ Feature Extractor/extract_features.py
@@ -5,19 +5,26 @@ from tfkerassurgeon.operations import delete_layer
 import os
 import cv2 as cv
 
+#load our trained model
 model=load_model('best_accuracy_our_model_refined_2_50x50.h5')
 
 print(model.summary())
+
 print(len(model.layers))
+
+#get last dense layer
 layer_1 = model.layers[14]
+
+#delete last dense layer using keras-surgeon
 model=delete_layer(model,layer_1)
 print(model.summary())
+
+#save updated model
 model.save('feature_extractor_1.h5')
-#load data
+
+# traverse through folders containing extracted frames from YawDD dataset, load them and add labels
 #1 is yawning
 
-#x_train_before=[]
-#y_train_before=[]
 x_before=[]
 y_before=[]
 
@@ -42,24 +49,19 @@ for image in os.listdir(path):
 	x_before.append(img)
 	y_before.append(0)
 
+#convert to numpy
+
 x_before=np.array(x_before)
 y_before=np.array(y_before)
-#x_test_before=np.array(x_test_before)
-#y_test_before=np.array(y_test_before)
+
 print(x_before.shape, y_before.shape)
-#print(x_before.shape, y_test_before.shape)
 
 #run model
-
-
 x_after=model.predict(x_before)
-#x_test_after=model.predict(x_test_before)
 y_after=y_before
-#y_test_after=y_test_before
 
 print(x_after.shape, y_after.shape)
-#print(x_test_after.shape, y_test_after.shape)
+
+#save the extracted features and corresponding labels
 np.save('x_all_1.npy',x_after)
 np.save('y_all_1.npy',y_after)
-#np.save('x_test.npy', x_test_after)
-#np.save('y_test.npy', y_test_after)
